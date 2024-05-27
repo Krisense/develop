563562088c2207d3ef5d8a0f319b4368724a9b95
Krisense <kristian.kristian.2014@mail.ru>
2024-05-27 15:01:45 +0300

    Added basic console calculator

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operation (+, -, *, /):");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
